688ac0a3a6446ec8f2d141fc72659c3f6f17e2ab
JSillero <46518912+JSillero@users.noreply.github.com>
2019-02-19 10:40:51 +0100

    nice
    nice

--- Fraccion.py
+++ Fraccion.py
@@ -7,59 +7,59 @@ Created on 17 ene. 2019
 class Fraccion:
     def __init__(self, den, num ):
         self.denominador=den;
-        self.numerador=num;
+        self.__numerador=num;
     
     def fraccionVista(self):
-        print(self.numerador,"/",self.denominador)
+        print(self.__numerador,"/",self.denominador)
         
     def getValues(self):
-        print("El valor del numerador es:" ,self.numerador," y el del denominador: ", self.denominador )
+        print("El valor del __numerador es:" ,self.__numerador," y el del denominador: ", self.denominador )
         
     def setValues(self,den,num):
         if den!=0:
-            print("0 no es un denominador valido, el numerador se guardará efectivamente  pero el denominador no.")
+            print("0 no es un denominador valido, el __numerador se guardará efectivamente  pero el denominador no.")
         else:
             self.denominador=den
-        self.numerador=num
+        self.__numerador=num
     
     def valorReal(self):
-        return (self.numerador/self.denominador)
+        return (self.__numerador/self.denominador)
     
     def multiplicar(self,mul):
-        self.numerador*=mul
+        self.__numerador+=mul*self.denominador
         
     def sumar(self, sum):
         if(self.denominador==sum.denominador):
-            self.numerador+=sum.numerador
+            self.__numerador+=sum.__numerador
         else:
-            sum.numerador*=self.denominador
-            self.numerador*=sum.denominador
+            sum.__numerador*=self.denominador
+            self.__numerador*=sum.denominador
             self.denominador+=sum.denominador
-            self.numerador+=sum.numerador
+            self.__numerador+=sum.__numerador
     
     def restar(self, sum):
-        if(self.denomidor==sum.denominador):
-            self.numerador-=sum.numerador
+        if(self.denominador==sum.denominador):
+            self.__numerador-=sum.__numerador
         else:
-            sum.numerador*=self.denominador
-            self.numerador*=sum.denominador
+            sum.__numerador*=self.denominador
+            self.__numerador*=sum.denominador
             self.denominador-=sum.denominador
-            self.numerador-=sum.numerador 
+            self.__numerador-=sum.__numerador 
             
     def multiplicarFrac(self, mul):
-        self.numerador*=mul.numerador
+        self.__numerador*=mul.__numerador
         self.denominador*mul.denominador
     
     def simplificar(self):
-        aux1=self.numerador
+        aux1=self.__numerador
         
-        i=0
+        i=1
         while(i<aux1/2):
-            i+=1
+            i
             while():
-                if(self.numerador%i==0 & self.denominador==0):
-                    self.numerador= self.numerador/i
+                if(self.__numerador%i==0 & self.denominador%i==0):
+                    self.__numerador= self.__numerador/i
                     self.denominador= self.denominador/i
                 else:
                     break
-        self.getValues
+        
